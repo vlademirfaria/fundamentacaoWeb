59b898308ace8f281bf1563b964928f22401a963
vlademirfaria <vemir38@hotmail.com>
2024-10-29 21:19:09 -0300

    01

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-window.onload = function() {
+window.onload = function () {
     const params = new URLSearchParams(window.location.search);
-    
+    const output = document.getElementById('output');
+
     if (params.has('name') && params.has('email') && params.has('age')) {
-        const output = document.getElementById('output');
         output.innerHTML = `
             <p>Nome: ${params.get('name')}</p>
             <p>Email: ${params.get('email')}</p>
@@ -11,19 +11,18 @@ window.onload = function() {
     } else {
         // Exibição de mensagem de erro caso algum dado esteja faltando
         console.error("Um ou mais parâmetros estão faltando.");
-        const output = document.getElementById('output');
         output.innerHTML = "Houve um erro ao carregar os dados.";
     }
+
+    const wrapper = document.querySelector('.wrapper');
+    const registerLink = document.querySelector('.register-link');
+    const loginLink = document.querySelector('.login-link');
+
+    registerLink.onclick = () => {
+        wrapper.classList.add('active');
+    };
+
+    loginLink.onclick = () => {
+        wrapper.classList.remove('active');
+    };
 };
-
-const wrapper = document.querySelector('.wrapper');
-const registerLink = document.querySelector('.register-link');
-const loginLink = document.querySelector('.login-link');
-
-registerLink.onclick = () => {
-    wrapper.classList.add('active');
-}
-
-loginLink.onclick = () => {
-    wrapper.classList.remove('active');
-}
